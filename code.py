ea0b37c909669c0ad49218c3a66bcc429ba11cf2
Hamad Dwairi <98936288+hamadbd@users.noreply.github.com>
2023-03-06 10:32:56 +0300

    Update code.py

--- code.py
+++ code.py
@@ -4,6 +4,17 @@ from tensorflow.keras.datasets import cifar10
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPooling2D
 from tensorflow.keras.utils import to_categorical
+import requests
+from io import BytesIO
+from PIL import Image
+
+# Define function to download image from URL and convert it to a NumPy array
+def download_and_convert_image(url):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+    img = img.resize((32, 32))
+    img_array = np.array(img)
+    return img_array
 
 # Load the CIFAR-10 dataset
 (X_train, y_train), (X_test, y_test) = cifar10.load_data()
@@ -29,10 +40,17 @@ model.add(Dense(10, activation='softmax'))
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
+# Define function to continuously read live data and make predictions
+def predict_from_live_data():
+    while True:
+        url = 'https://picsum.photos/200'
+        img_array = download_and_convert_image(url)
+        img_array = np.expand_dims(img_array, axis=0)
+        prediction = model.predict(img_array)
+        print(prediction)
+
 # Train the model
 model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
 
-# Evaluate the model on the test set
-test_loss, test_acc = model.evaluate(X_test, y_test)
-print('Test loss:', test_loss)
-print('Test accuracy:', test_acc)
+# Start making predictions from live data
+predict_from_live_data()
